334c07d39e54fa942517ea096bf90157b76b319a
yonguk <kimyu9827@naver.com>
2024-01-19 16:36:39 +0900

    init project

---
+++ src/main/java/com/bitharmony/comma/CommaApplication.java
@@ -0,0 +1,13 @@
+package com.bitharmony.comma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitharmony/comma/CommaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitharmony.comma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
