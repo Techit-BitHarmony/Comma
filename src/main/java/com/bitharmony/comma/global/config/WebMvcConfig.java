630ef6f5adbfac32673c15196e2b5f7b17b3f227
yonguk <kimyu9827@naver.com>
2024-01-19 16:58:03 +0900

    init: project start
    프로젝트 시작 및 글로벌 파일 복붙

---
+++ src/main/java/com/bitharmony/comma/global/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.bitharmony.comma.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
