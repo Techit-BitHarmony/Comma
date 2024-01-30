f3a9ab7da43bd69cef18237ee85dbca64579c61a
changhyun9 <ckdgus1713@naver.com>
2024-01-30 13:13:25 +0900

    refactor : Jwt 생성, 유효성 검사, Spring Security config 수정
    Jwt - Jwt 관련 JWtUtil, JwtAuthenticationFilter 수정
    Spring Security - security config file 수정
    Excpetion class - Custom Exception class 추가

--- src/main/java/com/bitharmony/comma/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/bitharmony/comma/global/security/JwtAuthenticationFilter.java
@@ -1,42 +1,45 @@
 package com.bitharmony.comma.global.security;
 
+import com.bitharmony.comma.global.util.JwtUtil;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.util.Arrays;
-import java.util.Optional;
-
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    //private final MemberService memberService;
+    private final JwtUtil jwtUtil;
 
     @Override
     @SneakyThrows
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
-        String apiKey = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            Optional<Cookie> rqCookie = Arrays.stream(cookies)
-                    .filter(cookie -> cookie.getName().equals("accessToken"))
-                    .findFirst();
-            if (rqCookie.isPresent()) {
-                apiKey = rqCookie.get().getValue();
-            }
-        }
 
-        if (apiKey != null) {
-            //SecurityUser user = memberService.getUserFromApiKey(apiKey);
-            //setAuthentication(user);
+        String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("accessToken = {}", accessToken);
+
+        if (accessToken != null && jwtUtil.validToken(accessToken)) {
+            Map<String, Object> userData = jwtUtil.getUserData(accessToken);
+            long id = Long.parseLong((String) userData.get("id"));
+            String username = (String) userData.get("username");
+
+            Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+            // Jwt 인증을 성공한 객체에 임의로 USER 역할을 부여하여, 인가를 허용.
+
+            setAuthentication(new SecurityUser(id, username, "", authorities));
         }
 
         filterChain.doFilter(request, response);
