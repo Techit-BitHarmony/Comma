2ed9b88db6b3e08ca54ba4e3f9ca3f5b11adff25
조유민 <nationmilo@gmail.com>
2024-01-30 17:41:37 +0900

    Album (#5)
    * feat : Init 및 Album 관련 엔티티 추가
    
    Init 및 Album 관련 Album , AlbumLike , StreamingCount Entity 추가
    
    * feat : 앨범 컨트롤러 및 생성 DTO 추가
    
    앨범 관련 스크립트 생성 Controller , CreateRequest
    
    * feat : 앨범 생성 기능 추가
    
    앨범 생성 관련 Controller, Service, Repository 수정
    
    * feat : amazonS3 ncp datastorage 관련 Configuration 추가
    
    NcpConfig 관련 설정 util 추가
    
    * feat : 앨범 생성 페이지 추가
    
    앨범 생성 페이지 추가 및 package 위치 변경
    
    * feat : 앨범 이미지 UUID로 저장 및 폴더에 저장
    
    custom을 활용해 이후 서버 경로에 파일 저장 UUID는 db에 저장하여 분류
    
    * feat : ncp object storage 연동 음원 이미지 업로드 추가
    
    ncp object storage 연동해서 musicstorage 폴더에 이미지 업로드 기능 -
    db에는 url저장으로 구현
    
    * feat : Album Edit 기능 샘플 코드 추가
    
    edit 샘플 코드 작성 및 showAlbumForm Dto 반환 추가
    
    * feat : ncp image optimizer을 활용한 이미지 변경 추가
    
    object storage에서 ncp image optimizer cdn url로 연동해서 이미지 반환
    
    * feat : 앨범 수정 / 삭제 기능 추가
    
    앨범 수정 및 삭제 기능
    
    * fix : 앨범 dto가 아닌 Entity로 가져오는 버그 수정
    
    albumToResponseDto 추가
    
    * refact : ncp 설정 관련 값들을 Ncp Properties로 변경 및 리펙토링
    
    ncp properties(component)에서 ncp에 필요한 값을 받아서 처리
    
    * refact : 음악 파일 및 이미지 관련 코드 리펙토링
    
    fileservice 및 ncp에서 삭제 요소 추가 - multipartfiles 오류 수정 등
    
    * refact : album controller 리펙토링
    
    albumcontroller dto 리펙토링
    
    * refact : AlbumService 및 Controller 리펙토링
    
    코드 최적화 및 null 단순화
    
    * refact : getAlbumById 코드 수정
    
    서비스에서 오류 처리
    
    * fix : application-custom ignore 정보 수정
    
    ignore 미작동으로 인해 yml이 같이 업데이트됨.
    
    * refact : albumResponse Builder
    
    생성자 대신 빌더로 변경
    
    * refact : ncp 관련 코드 리펙토링
    
    여러 ncp 관련 코드 리펙토링
    
    * fix : yml 파일 수정
    
    yml 파일 수정

---
+++ src/main/java/com/bitharmony/comma/album/album/entity/Album.java
@@ -0,0 +1,69 @@
+package com.bitharmony.comma.album.album.entity;
+
+import com.bitharmony.comma.album.album.dto.AlbumEditRequest;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Album {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "album_id")
+	private Long id;
+
+	// @ManyToOne(name = "member_id")(fetch = FetchType.LAZY)
+	// private Member member;
+
+	@Column(nullable = false, length = 50)
+	private String albumname;
+
+	@Column(nullable = false, length = 20)
+	private String genre;
+
+	@Column(nullable = false, columnDefinition = "boolean default false")
+	private boolean license;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String licenseDescription;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String filePath;
+
+	@Column(columnDefinition = "TEXT")
+	private String imagePath;
+
+	@Column(nullable = false, columnDefinition = "boolean default false")
+	private boolean permit;
+
+	@Column(nullable = false, columnDefinition = "int default 0")
+	private int price;
+
+	// @OneToMany(fetch = FetchType.LAZY)
+	// private List<AlbumLike> albumLike;
+
+	public void updateImageUrl(String url) { this.imagePath = url; }
+
+	public void updateFileUrl(String url) { this.filePath = url; }
+
+	public void update(AlbumEditRequest request) {
+		this.albumname = request.albumname();
+		this.genre = request.genre();
+		this.license = request.license();
+		this.licenseDescription = request.licenseDescription();
+		this.permit = request.permit();
+		this.price = request.price();
+	}
+}
