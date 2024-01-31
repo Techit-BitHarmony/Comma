673a51b9078ae9abbf72a4602632dcc36ae736b6
yonguk <kimyu9827@naver.com>
2024-02-01 01:13:37 +0900

    feature: quartz config creating

--- src/main/java/com/bitharmony/comma/donation/service/DonationRegularService.java
+++ src/main/java/com/bitharmony/comma/donation/service/DonationRegularService.java
@@ -1,12 +1,11 @@
 package com.bitharmony.comma.donation.service;
 
-import com.bitharmony.comma.donation.repository.DonationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 public class DonationRegularService {
-    private final DonationRepository donationRepository;
 
 }
---
+++ src/main/java/com/bitharmony/comma/global/config/QuartzSchedulerConfig.java
@@ -0,0 +1,11 @@
+package com.bitharmony.comma.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+public class QuartzSchedulerConfig {
+}
