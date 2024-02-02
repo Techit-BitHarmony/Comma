ea47aedaa6ebe1e15025f00f31777eba806ab94a
yonguk <kimyu9827@naver.com>
2024-02-02 16:42:53 +0900

    feature: 후원 시 크레딧 변경 로직 추가
    후원 할 때 후원자와 아티스트의 크레딧 보유량 업데이트 하는 로직 추가 및 크레딧 부족에 대한 예외처리 추가

--- src/main/java/com/bitharmony/comma/donation/service/DonationService.java
+++ src/main/java/com/bitharmony/comma/donation/service/DonationService.java
@@ -6,7 +6,9 @@ import com.bitharmony.comma.donation.dto.DonationRegularRequsetDto;
 import com.bitharmony.comma.donation.dto.DonationResponse;
 import com.bitharmony.comma.donation.entity.Donation;
 import com.bitharmony.comma.donation.repository.DonationRepository;
+import com.bitharmony.comma.global.exception.CreditShortageException;
 import com.bitharmony.comma.member.entity.Member;
+import com.bitharmony.comma.member.repository.MemberRepository;
 import com.bitharmony.comma.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,7 @@ import java.util.List;
 public class DonationService {
     private final DonationRepository donationRespository;
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
     public void donateOnceToArtist(DonationOnceRequestDto dto) {
         Member patron = memberService.getMemberByUsername(dto.patronName());
@@ -26,8 +29,6 @@ public class DonationService {
 
         //credit 부족한지 확인
         checkCredit(patron, dto.amount());
-
-        //멤버 엔티티에서 credit 차감
 
         //donation 엔티티 생성 및 저장
         Donation donation = Donation.builder()
@@ -39,37 +40,50 @@ public class DonationService {
                 .build();
         donationRespository.save(donation);
 
-        //아티스트에게 크레딧 바로 전달?
+        //후원자의 크레딧을 아티스트에게 전달
+        countCredit(patron, artist, dto.amount());
 
         // 결과 반환
 
     }
 
-    public void donateRegularToArtist(DonationRegularRequsetDto dto){
-        Member patron = memberService.getMemberByUsername(dto.patronName());
-        Member artist = memberService.getMemberByUsername(dto.artistName());
+    public void donateRegularToArtist(DonationRegularRequsetDto dto) {
+        Member patron = memberService.getMemberByUsername(dto.getPatronName());
+        Member artist = memberService.getMemberByUsername(dto.getArtistName());
 
         //credit 부족한지 확인
-        checkCredit(patron, dto.amount());
-
-        //멤버 엔티티에서 credit 차감
+        checkCredit(patron, dto.getAmount());
 
         //donation 엔티티 생성 및 저장
         Donation donation = Donation.builder()
                 .artistUsername(artist.getUsername())
                 .patron(patron)
-                .amount(dto.amount())
+                .amount(dto.getAmount())
                 .message("")
-                .anonymous(dto.anonymous())
+                .anonymous(dto.isAnonymous())
                 .build();
         donationRespository.save(donation);
+
+        //후원자의 크레딧을 아티스트에게 전달
+        countCredit(patron, artist, dto.getAmount());
+    }
+
+    // donation에서 memberRepostiory에 접근 하는 방식이 옳은가?
+    private void countCredit(Member patron, Member artist, Integer amount) {
+        patron = patron.toBuilder().credit(patron.getCredit() - amount).build();
+        artist = artist.toBuilder().credit(artist.getCredit() + amount).build();
+
+        memberRepository.save(patron);
+        memberRepository.save(artist);
     }
 
     private void checkCredit(Member patron, Integer amount) {
         if (patron.getCredit() < amount) {
             // 후원 프로세스 멈추고 금액부족 메세지 반환
+            throw new CreditShortageException("크레딧이 부족합니다.");
         }
     }
+
     // 회원 인증 필요
     // 아티스트가 받은 내역 조회 할 때
     public DonationResponse getDonationListByArtistUsername(String username) {
@@ -93,6 +107,7 @@ public class DonationService {
         }
         return DonationResponse.of("후원내역 반환.", responseDtoList);
     }
+
     // 회원 인증 필요
     // 후원자가 준 내역 확인 할 때
     public DonationResponse getDonationListByPatron(Member patron) {
--- src/main/java/com/bitharmony/comma/global/exception/CreditShortageException.java
+++ src/main/java/com/bitharmony/comma/global/exception/CreditShortageException.java
@@ -1,2 +1,12 @@
-package com.bitharmony.comma.global.exception;public class CreditShortageException {
+package com.bitharmony.comma.global.exception;
+
+public class CreditShortageException extends CommaException {
+    public CreditShortageException(String message) {
+        super(message);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 400;
+    }
 }
