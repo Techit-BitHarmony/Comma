2e833b988289cc8c7ba2a797b4afa939c8bcff23
Sanghyuk Park <dropthegit@gmail.com>
2024-02-05 23:00:25 +0900

    refact : ArticleGetResponse Page<ArticleDto> 로 변경
    page 크기 10으로 설정
    Page<ArticleDto>로 반환

--- src/main/java/com/bitharmony/comma/global/config/QuartzSchedulerConfig.java
+++ src/main/java/com/bitharmony/comma/global/config/QuartzSchedulerConfig.java
@@ -1,50 +1,50 @@
-package com.bitharmony.comma.global.config;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.concurrent.ThreadPoolExecutor;
-
-@Slf4j
-@RequiredArgsConstructor
-@Configuration
-public class QuartzSchedulerConfig {
-    private final DataSource dataSource;
-    private final PlatformTransactionManager platformTransactionManager;
-
-    @Bean
-    public SchedulerFactoryBean schedulerFactoryBean() {
-        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
-
-        schedulerFactoryBean.setDataSource(dataSource);
-        schedulerFactoryBean.setOverwriteExistingJobs(true);
-        schedulerFactoryBean.setAutoStartup(true);
-        schedulerFactoryBean.setTransactionManager(platformTransactionManager);
-        schedulerFactoryBean.setQuartzProperties(quartzProperties());
-
-        return schedulerFactoryBean;
-    }
-
-    private Properties quartzProperties() {
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        propertiesFactoryBean.setLocation(new ClassPathResource("application.yml"));
-        Properties properties = null;
-        try {
-            propertiesFactoryBean.afterPropertiesSet();
-            properties = propertiesFactoryBean.getObject();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return properties;
-    }
-}
+//package com.bitharmony.comma.global.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.config.PropertiesFactoryBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+//import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//import javax.sql.DataSource;
+//import java.io.IOException;
+//import java.util.Properties;
+//import java.util.concurrent.ThreadPoolExecutor;
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//@Configuration
+//public class QuartzSchedulerConfig {
+//    private final DataSource dataSource;
+//    private final PlatformTransactionManager platformTransactionManager;
+//
+//    @Bean
+//    public SchedulerFactoryBean schedulerFactoryBean() {
+//        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+//
+//        schedulerFactoryBean.setDataSource(dataSource);
+//        schedulerFactoryBean.setOverwriteExistingJobs(true);
+//        schedulerFactoryBean.setAutoStartup(true);
+//        schedulerFactoryBean.setTransactionManager(platformTransactionManager);
+//        schedulerFactoryBean.setQuartzProperties(quartzProperties());
+//
+//        return schedulerFactoryBean;
+//    }
+//
+//    private Properties quartzProperties() {
+//        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+//        propertiesFactoryBean.setLocation(new ClassPathResource("application.yml"));
+//        Properties properties = null;
+//        try {
+//            propertiesFactoryBean.afterPropertiesSet();
+//            properties = propertiesFactoryBean.getObject();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return properties;
+//    }
+//}
