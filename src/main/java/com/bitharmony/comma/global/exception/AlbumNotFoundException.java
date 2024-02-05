5927e0075e3c5a3d14a7c5cf8908007e3d2fd152
조유민 <nationmilo@gmail.com>
2024-02-05 14:12:09 +0900

    refact : Exception 관련 주석 제거
    주석 제거

--- src/main/java/com/bitharmony/comma/global/exception/AlbumNotFoundException.java
+++ src/main/java/com/bitharmony/comma/global/exception/AlbumNotFoundException.java
@@ -12,16 +12,3 @@ public class AlbumNotFoundException extends CommaException {
 		return 400;
 	}
 }
-
-/*** 사용법
- ~~~ Exception 과 같이 정의
-
- throw new AlbumNotFoundException();
-
- 아래와 같이 Client로 전달 됨.
-
- {
- code : 400
- message : "앨범을 찾을 수 없습니다."
- }
- ***/
