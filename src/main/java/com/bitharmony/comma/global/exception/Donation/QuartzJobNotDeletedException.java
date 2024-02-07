a778e1a1f1a037b0d0927bcdc5a974124c82a2b8
yonguk <kimyu9827@naver.com>
2024-02-07 16:30:01 +0900

    feature: create exceptions

--- src/main/java/com/bitharmony/comma/global/exception/Donation/QuartzJobNotDeletedException.java
+++ src/main/java/com/bitharmony/comma/global/exception/Donation/QuartzJobNotDeletedException.java
@@ -1,2 +1,17 @@
-package com.bitharmony.comma.global.exception.Donation;public class QuartzJobNotDeletedException {
+package com.bitharmony.comma.global.exception.Donation;
+
+import com.bitharmony.comma.global.exception.CommaException;
+
+public class QuartzJobNotDeletedException extends CommaException {
+
+    private static String MESSAGE = "등록된 작업을 삭제하는데 실패했습니다.";
+
+    public QuartzJobNotDeletedException() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 0;
+    }
 }
