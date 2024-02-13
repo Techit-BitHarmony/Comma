ce9f4f05ae667b4f6e85cee575ef0dae8fc22fec
changhyun9 <ckdgus1713@naver.com>
2024-02-13 14:50:56 +0900

    fix : frontend에서 login, join 시 error message 오류 수정, cors 설정 변경
    WebMvcConfig - CORS 설정 변경, /api/**에서 /**로 변경
    Exception - error message 오류 수정, Http status code 수정
    DTO - Validation 시 default error message 설정

--- src/main/java/com/bitharmony/comma/global/config/WebMvcConfig.java
+++ src/main/java/com/bitharmony/comma/global/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("*")
                 .allowedHeaders("*")
