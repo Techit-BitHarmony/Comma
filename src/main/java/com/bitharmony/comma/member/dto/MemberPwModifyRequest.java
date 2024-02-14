935011fa21bd8163c4b9a19393d7ab614ee5633f
changhyun9 <ckdgus1713@naver.com>
2024-02-14 13:17:55 +0900

    i# 제목: 아래에 작성, 제목 끝 마침표 금지, 개조식 작성(과거형 금지) refactor : DTO refactoring
    DTO - refactoring

--- src/main/java/com/bitharmony/comma/member/dto/MemberPwModifyRequest.java
+++ src/main/java/com/bitharmony/comma/member/dto/MemberPwModifyRequest.java
@@ -1,11 +1,11 @@
 package com.bitharmony.comma.member.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record MemberPwModifyRequest(
-        @NotNull(message = "Password을 입력해주세요.") String passwordModify,
-        @NotNull(message = "Password 확인을 입력해주세요.") String passwordModifyCheck
+        @NotBlank(message = "Password을 입력해주세요.") String passwordModify,
+        @NotBlank(message = "Password 확인을 입력해주세요.") String passwordModifyCheck
 ) {
 }
