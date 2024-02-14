09164f6c83abd253741800c87923c8b4bfde3631
Sanghyuk Park <dropthegit@gmail.com>
2024-02-14 20:05:19 +0900

    securityConfig Comments 엔드포인트 수정

--- src/main/java/com/bitharmony/comma/credit/withdraw/controller/WithdrawController.java
+++ src/main/java/com/bitharmony/comma/credit/withdraw/controller/WithdrawController.java
@@ -6,6 +6,7 @@ import com.bitharmony.comma.credit.withdraw.entity.Withdraw;
 import com.bitharmony.comma.member.entity.Member;
 import com.bitharmony.comma.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
