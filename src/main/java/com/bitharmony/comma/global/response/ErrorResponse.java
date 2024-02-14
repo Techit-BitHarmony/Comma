ef5a5760054ac9c8aef86c09e9fbe715298d0bb8
ppusda <ppusda1234@gmail.com>
2024-02-14 15:07:05 +0900

    refactor: Valid 필드 처리 수정

--- src/main/java/com/bitharmony/comma/global/response/ErrorResponse.java
+++ src/main/java/com/bitharmony/comma/global/response/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.bitharmony.comma.global.response;
 
+import java.util.Map;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,10 +8,12 @@ import lombok.Getter;
 public class ErrorResponse {
     private final String code;
     private final String message;
+    private final Map<String, String> validMessages;
 
     @Builder
-    public ErrorResponse(String code, String message) {
+    public ErrorResponse(String code, String message, Map<String, String> validMessages) {
         this.code = code;
         this.message = message;
+        this.validMessages = validMessages;
     }
 }
